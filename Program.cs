f2d3131eaba2a54cbc89212ca4bcd2c1294bed0d
AleksandrSergeenko <122687809+AleksandrSergeenko@users.noreply.github.com>
2023-03-16 20:25:40 +0300

    Addрешение  task64

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
  */
 
-/* int N = InputNumbers("Введите N: ");
+int N = InputNumbers("Введите N: ");
 int count = 2;
 PrintNumber(N, count);
 Console.Write(1);
@@ -21,69 +21,3 @@ int InputNumbers(string input)
     int output = Convert.ToInt32(Console.ReadLine());
     return output;
 }
- */
-
-/* 
-Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30
- */
-/* 
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int temp = m;
-
-if (m > n) 
-{
-    m = n; 
-    n = temp;
-}
-
-PrintSumm(m, n, temp = 0);
-
-void PrintSumm(int m, int n, int summ)
-{
-    summ = summ + n;
-    if (n <= m)
-    {
-        Console.Write($"Сумма элементов = {summ} ");
-        return;
-    }
-    PrintSumm(m, n - 1, summ);
-}
-
-int InputNumbers(string input) 
-{
-    Console.Write(input);
-    int output = Convert.ToInt32(Console.ReadLine());
-    return output;
-}
- */
-
-/* 
-Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-m = 2, n = 3 -> A(m,n) = 9
-m = 3, n = 2 -> A(m,n) = 29
- */
-/* 
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-
-int functionAkkerman = Ack(m, n);
-
-Console.Write($"A(m,n) = {functionAkkerman} ");
-
-int Ack(int m, int n)
-{
-    if (m == 0) return n + 1;
-    else if (n == 0) return Ack(m - 1, 1);
-    else return Ack(m - 1, Ack(m, n - 1));
-}
-
-int InputNumbers(string input) 
-{
-    Console.Write(input);
-    int output = Convert.ToInt32(Console.ReadLine());
-    return output;
-}
- */
